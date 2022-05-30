131add83a53e97003c24ca7572ce7155f0dd0daa
Eliel Siqueira <elielkaragua2016@gmail.com>
2022-05-30 17:55:50 -0300

    fazer o login

--- routes.php
+++ routes.php
@@ -1,4 +1,11 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT");
+    header("Access-Control-Allow-Headers: Access-Token");
+    die;
+}
+
 require('config.php'); 
 // cria a constante caminho padrão
 $url = $_SERVER['REQUEST_URI']; // pega o que está na url
